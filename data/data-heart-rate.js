231f44371b0eeb6a63d85e202619ada2df16e429
Jordy van der Poel <jordyvanderpoel@gmail.com>
2017-05-23 22:29:26 +0200

    Styling heartrate-page

--- data/data-heart-rate.js
+++ data/data-heart-rate.js
@@ -1,153 +1,189 @@
-var month = [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+var day = [{
+            period: '2017-06-28 10:00',
+            heartrate: 70
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2017-06-28 11:00',
+            heartrate: 71
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2017-06-28 12:00',
+            heartrate: 74
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2017-06-28 13:00',
+            heartrate: 73
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2017-06-28 14:00',
+            heartrate: 75
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2017-06-28 15:00',
+            heartrate: 82
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2017-06-28 16:00',
+            heartrate: 88
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2017-06-28 17:00',
+            heartrate: 91
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2017-06-28 18:00',
+            heartrate: 90
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2017-06-28 19:00',
+            heartrate: 84
+        }, {
+            period: '2017-06-28 20:00',
+            heartrate: 81
+        }, {
+            period: '2017-06-28 21:00',
+            heartrate: 85
+        }, {
+            period: '2017-06-28 22:00',
+            heartrate: 80
+        }, {
+            period: '2017-06-28 23:00',
+            heartrate: 77
+        }, {
+            period: '2017-06-29 00:00',
+            heartrate: 72
+        }, {
+            period: '2017-06-29 01:00',
+            heartrate: 68
+        }, {
+            period: '2017-06-29 02:00',
+            heartrate: 63
+        }, {
+            period: '2017-06-29 03:00',
+            heartrate: 68
+        }, {
+            period: '2017-06-29 04:00',
+            heartrate: 66
+        }, {
+            period: '2017-06-29 05:00',
+            heartrate: 65
+        }, {
+            period: '2017-06-29 06:00',
+            heartrate: 68
+        }, {
+            period: '2017-06-29 07:00',
+            heartrate: 72
+        }, {
+            period: '2017-06-29 08:00',
+            heartrate: 83
+        }, {
+            period: '2017-06-29 09:00',
+            heartrate: 93
+        }, {
+            period: '2017-06-29 10:00',
+            heartrate: 98
         }];
 var week = [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2017-06-23',
+            heartrate: 79
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2017-06-24',
+            heartrate: 76
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2017-06-25',
+            heartrate: 83
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2017-06-26',
+            heartrate: 82
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2017-06-27',
+            heartrate: 85
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2017-06-28',
+            heartrate: 88
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2017-06-29',
+            heartrate: 84
         }];
-var day = [{
-            period: '2010 Q1',
-            iphone: 0,
-            ipad: null,
-            itouch: 0
+var month = [{
+            period: '2017-05-31',
+            heartrate: 87
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2017-06-01',
+            heartrate: 93
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2017-06-02',
+            heartrate: 88
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2017-06-03',
+            heartrate: 83
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2017-06-04',
+            heartrate: 81
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2017-06-05',
+            heartrate: 72
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2017-06-06',
+            heartrate: 79
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2017-06-07',
+            heartrate: 87
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2017-06-08',
+            heartrate: 83
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2017-06-09',
+            heartrate: 88
+        }, {
+            period: '2017-06-10',
+            heartrate: 81
+        }, {
+            period: '2017-06-11',
+            heartrate: 85
+        }, {
+            period: '2017-06-12',
+            heartrate: 91
+        }, {
+            period: '2017-06-13',
+            heartrate: 78
+        }, {
+            period: '2017-06-14',
+            heartrate: 82
+        }, {
+            period: '2017-06-15',
+            heartrate: 89
+        }, {
+            period: '2017-06-16',
+            heartrate: 86
+        }, {
+            period: '2017-06-17',
+            heartrate: 80
+        }, {
+            period: '2017-06-18',
+            heartrate: 82
+        }, {
+            period: '2017-06-19',
+            heartrate: 83
+        }, {
+            period: '2017-06-20',
+            heartrate: 84
+        }, {
+            period: '2017-06-21',
+            heartrate: 93
+        }, {
+            period: '2017-06-22',
+            heartrate: 86
+        }, {
+            period: '2017-06-23',
+            heartrate: 79
+        }, {
+            period: '2017-06-24',
+            heartrate: 76
+        }, {
+            period: '2017-06-25',
+            heartrate: 83
+        }, {
+            period: '2017-06-26',
+            heartrate: 82
+        }, {
+            period: '2017-06-27',
+            heartrate: 85
+        }, {
+            period: '2017-06-28',
+            heartrate: 88
+        }, {
+            period: '2017-06-29',
+            heartrate: 84
         }];
